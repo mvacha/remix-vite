9e8a5dba83eb821a39e398cbe6d8e1835d9b3b82
Michal Vácha <michal.vacha@live.com>
2023-10-25 21:47:00 +0200

    tailwind (working in dev, not in prod)

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -9,11 +8,11 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import styles from "./tailwind.css?url";
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
+
   return (
     <html lang="en">
       <head>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useSearchParams } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +9,17 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [query, setQuery] = useSearchParams();
+
+  const clickAction = () =>
+    setQuery((q) => ({
+      foo: "bar",
+    }));
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <a onClick={clickAction}>Set query param</a>
+    </>
   );
 }
